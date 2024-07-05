bb9eee58637d7b23217707aa76008908afb4beea
seleniumriyaz <selenium.riyaz.shared@gmail.com>
2024-07-05 10:14:49 +0530

    Java Season Notes and Programs till july 5th

---
+++ Hello.java
@@ -0,0 +1,20 @@
+//This is a java program to print hello world on console 
+/*   lkjlkjkljj4
+kjkljkljlkjlk
+
+khkjklkjkllkjkj*/
+class Hello
+{
+
+public static void main(String[] args)
+{
+
+//this is statement to print something on console
+System.out.println("Hello World ...");
+
+}
+
+
+}
+
+//end of program
