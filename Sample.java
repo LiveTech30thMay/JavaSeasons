e07fcf4295e681f1a3d51175bc7baead19699e09
seleniumriyaz <selenium.riyaz.shared@gmail.com>
2024-07-08 10:19:53 +0530

    Java Season Notes and Programs till july 8th

---
+++ Sample.java
@@ -0,0 +1,22 @@
+//import java.util.*;
+import java.util.Scanner;
+
+class Sample
+{
+
+public static void main(String[] args)
+{
+
+System.out.println("Hello ...");
+
+System.out.println("Enter some Integer");
+
+Scanner input=new Scanner(System.in);
+
+int x=input.nextInt();
+
+System.out.println("x is "+x);
+
+}
+
+}
